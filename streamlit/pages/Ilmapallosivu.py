abe1c4a72ef23a1083194381907464aef664990d
LassiJaara <74287707+LassiJaar@users.noreply.github.com>
2025-11-25 14:30:12 +0200

    Delete streamlit/pages/Ilmapallosivu.py

--- streamlit/pages/Ilmapallosivu.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def bab():
- st.balloons()
-
-st.title('Tavallaan tässä on käytetty API:a.')
-st.markdown('Ainakin Streamlitin sivuilla Button- ja Balloons-ominaisuus oli li>
-st.button('Tästä tulee ilmapalloja', on_click=bab)
