8fdde08e136683a47bd9acaf3415587dbc27c615
LassiJaara <74287707+LassiJaar@users.noreply.github.com>
2025-11-26 18:48:44 +0200

    Fix app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import mysql.connector
 import pandas as pd
-conn = mysql.connector.connect(host='localhost', user='exampleuser', password='>
+conn = mysql.connector.connect(host='localhost', user='exampleuser', password=='SALASANA', database='weather_db')
 df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50',
 conn)
 conn.close()
 st.title('Säädata Helsingistä, joka on haettu 15 minuutin välein ja tallennettu>
 st.dataframe(df)
-st.markdown('Line_chart on Streamlitin mukaan myös API. (???)')
 st.line_chart(data=df, x='timestamp', y=['temperature'])
 
 
