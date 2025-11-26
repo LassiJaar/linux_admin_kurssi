069e24b0f339a891dae46588095c0d09f1cb5d90
LassiJaara <74287707+LassiJaar@users.noreply.github.com>
2025-11-26 18:51:54 +0200

    Fetch_weather.py korjattu

--- cron_assignment/fetch_weather.py
+++ cron_assignment/fetch_weather.py
@@ -4,16 +4,16 @@ import mysql.connector
 from datetime import datetime
 API_KEY = 'OMA-API-KEY'
 CITY = 'Helsinki'
-URL = f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY>
-conn = mysql.connector.connect(host='localhost', user='exampleuser', password='>
+URL = f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+conn = mysql.connector.connect(host='localhost', user='exampleuser', password='SALASANA', database='weather_db')
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (id INT AUTO_INCREMEN>
+cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (id INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(50), temperature FLOAT, description VARCHAR(100), timestamp DATETIME)''')
 response = requests.get(URL)
 data = response.json()
 temp = data['main']['temp']
 desc = data['weather'][0]['description']
 timestamp = datetime.now()
-cursor.execute('INSERT INTO weather_data (city, temperature, description, times>
+cursor.execute('INSERT INTO weather_data (city, temperature, description, timestamp) VALUES (%s, %s, %s, %s)', (CITY, temp, desc, timestamp))
 conn.commit()
 cursor.close()
 conn.close()
