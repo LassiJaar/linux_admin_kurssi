6e03cff06aa1908f39b6bb7b919a9459c0b5141c
LassiJaara <74287707+LassiJaar@users.noreply.github.com>
2025-11-28 14:07:22 +0200

    Update lemp-app app.py

--- lemp-app/app.py
+++ lemp-app/app.py
@@ -17,23 +17,10 @@ def home():
  # Clean up
  cursor.close()
  conn.close()
- return f"""
-<head>
-<h1>LINUX KURSSIN NETTISIVUT!</h1>
-<p1>MYSQL-kello on nyt {result[0]}</p1>
-</head>
+ with open('app.html', 'r') as ff:
+  html_string = ff.read()
+ return html_string
 
-<body>
-<br>
-<button onClick="history.go(0);">Tässä on nappula joka päivittää sivun!</button>
-<br>
-<a href="http://86.50.23.172/data-analysis">TÄÄLTÄ LÖYTYY DATA-ANALYSIS</a>
-<br>
-<a href="http://86.50.23.172/chat">TÄÄLTÄ LÖYTYY CHAT-PALVELU!</a>
-
-
-</body>
-"""
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000)
 
